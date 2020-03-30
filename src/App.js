30e515ac1bae7223dba6978b4861ade0ab510124
LiamPKenna <liampkenna@gmail.com>
2020-03-30 12:53:40 -0700

    Import component and wrap in Material-UI theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DisqusBox from './components/DisqusBox';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
+  const theme = createMuiTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <DisqusBox pageId={'testerino'} pageUrl={'testerino'} />
+    </ThemeProvider>
   );
 }
 

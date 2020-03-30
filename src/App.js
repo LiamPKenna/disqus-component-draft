43d640fb7a7a8a1c494dabfeb2d9c99e235261f4
LiamPKenna <liampkenna@gmail.com>
2020-03-30 13:23:20 -0700

    Add dark theme and update props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import DisqusBox from './components/DisqusBox';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 function App() {
-  const theme = createMuiTheme();
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
   return (
     <ThemeProvider theme={theme}>
-      <DisqusBox pageId={'testerino'} pageUrl={'testerino'} />
+      <DisqusBox pageId={'testerino'} pageUrl={'https://www.testerino.com/chat'} />
     </ThemeProvider>
   );
 }

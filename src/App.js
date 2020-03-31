c1b127c6def601c419905d75f31ba591201e4ba3
LiamPKenna <liampkenna@gmail.com>
2020-03-31 06:54:57 -0700

    Add colors to theme to verify function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import DisqusEmbed from './components/DisqusEmbed';
 import Card from '@material-ui/core/Card';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
 
 function App() {
   const theme = createMuiTheme({
     palette: {
       type: 'dark',
+      primary: purple,
+      secondary: green,
     },
   });
   return (

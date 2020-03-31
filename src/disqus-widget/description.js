761d42938f27fcb6553ee2d6e0c3b95b9d849331
LiamPKenna <liampkenna@gmail.com>
2020-03-31 11:24:32 -0700

    Add input fields for Website Name and Title
    Co-authored-by: Ali Amirhamzeh <aliamirhamzeh@gmail.com>

--- src/disqus-widget/description.js
+++ src/disqus-widget/description.js
@@ -1,7 +1,8 @@
 import { BlockType } from '@sightworks/datastore';
 
 export default () => {
-   const Block = new BlockType('disqus-widget', 'Disqus Widget Embed');
-   Block.addSingleLineText('URL', 'url');
+   const Block = new BlockType('disqus-widget', 'Disqus Widget Embed')
+      .addSingleLineText('Website Name (shortname)', 'websiteName')
+      .addSingleLineText('Title', 'blockTitle')
    return Block;
 };

88e2df5ee5b5fa453280fe77756e1a8cb2040364
LiamPKenna <liampkenna@gmail.com>
2020-03-31 06:29:20 -0700

    Add styled-components & useTheme for theme color

--- src/components/DisqusEmbed.jsx
+++ src/components/DisqusEmbed.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { DiscussionEmbed } from "disqus-react";
+import styled from "styled-components";
+import { useTheme } from "@material-ui/core/styles";
+
+const DisqusDiv = styled.div`
+  padding: 1rem;
+  font-family: ${props => props.theme.typography.fontFamily};
+  a {
+    color: ${props =>
+      props.theme.palette.secondary[props.dark ? "light" : "dark"]};
+  }
+`;
 
 const DisqusEmbed = props => {
   const { websiteName, pageUrl, blockTitle } = props;
+  const theme = useTheme();
+  console.log(theme);
 
   const disqusConfig = {
     url: pageUrl,
@@ -10,7 +23,11 @@ const DisqusEmbed = props => {
     title: blockTitle
   };
 
-  return <DiscussionEmbed shortname={websiteName} config={disqusConfig} />;
+  return (
+    <DisqusDiv theme={theme} dark={theme.palette.type === "dark"}>
+      <DiscussionEmbed shortname={websiteName} config={disqusConfig} />;
+    </DisqusDiv>
+  );
 };
 
 export default DisqusEmbed;
